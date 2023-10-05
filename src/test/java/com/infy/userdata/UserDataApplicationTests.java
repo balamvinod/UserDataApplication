6d37292c8d312558e0990a6ec34543abb39d17e9
bvinod.kumar02 <bvinod.kumar02@CHNSHL564543L.ad.infosys.com>
2023-10-05 14:29:30 +0530

    cChanges in pom.xml

--- src/test/java/com/infy/userdata/UserDataApplicationTests.java
+++ src/test/java/com/infy/userdata/UserDataApplicationTests.java
@@ -1,22 +1,32 @@
 package com.infy.userdata;
-
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.infy.userdata.exception.UserDataException;
 import com.infy.userdata.repository.UserDataRepository;
 import com.infy.userdata.service.UserDataService;
 import com.infy.userdata.service.UserDataServiceImpl;
-
-
+@SpringBootTest
 public class UserDataApplicationTests {
+@Mock
+private UserDataRepository userDataRepository;
 
-	
-	private UserDataRepository userDataRepository;
-	
+@InjectMocks
+private UserDataService userDataService = new UserDataServiceImpl();
 
 
-	private UserDataService userDataService = new UserDataServiceImpl();
-	
-	
-	
-	public void getDetailsByUserNameNoDetailsFound() {
-	
-	}
+@Test
+public void getDetailsByUserNameNoDetailsFound() {
+Mockito.when(userDataRepository.findByUserName(Mockito.anyString())).thenReturn(List.of());
+String userName = "some_user_name";
+UserDataException e= Assertions.assertThrows(UserDataException.class, ()-> userDataService.getDetailsByUserName(userName));
+Assertions.assertEquals("Service.NO_DETAILS_FOUND", e.getMessage());
+
+
+
+}
 }
